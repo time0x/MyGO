5b8fd01192fd3ecd49504188f6aa2e1811a25a53
time <wjhechen@gmail.com>
2018-03-07 10:25:27 +0800

    add example

---
+++ example/HelloWorld.go
@@ -0,0 +1,31 @@
+// 当前程序的包名
+package main
+
+// 导入的其他包名
+import (
+    "fmt"
+)
+
+// 常量的赋值
+const PAI = 3.1415926
+
+// 全局变量的声明与赋值
+var name = "time"
+
+// 一般类型赋值
+type age int
+
+// 结构的声明
+type goxx struct{}
+
+// 接口的声明
+type goxxx interface{}
+
+// 由main 函数作为程序入口启动
+func main() {
+    fmt.Println("hello world!")
+}
+
+
+
+
