15914872108008ad2d19e2507664de9910b90134
time <wjhechen@gmail.com>
2018-04-08 22:27:20 +0800

    fix note

---
+++ example/func.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := 1
+	// 参数是引用类型
+	func_c(a, 2, 3) // [1,2,3]
+	// 如果要修改参数，传递参数指针进去
+	func_d(&a)
+
+	// 函数也是一种类型
+	e := func_e
+	e() // func_e
+
+	//匿名函数
+	func_f := func() {
+		fmt.Println("匿名函数 f")
+	}
+	func_f()
+
+	// 闭包
+	closure := func_g(10)
+	fmt.Println(closure(2)) //12
+	fmt.Println(closure(6)) //16
+
+}
+
+func func_a(a int, b string) (int, string) {
+	// 返回 2个参数，int 类型和 string 类型
+	return a, b
+}
+
+// 3个参数都是int
+func func_b() (a, b, c int) {
+	// 直接赋值
+	a, b, c = 1, 2, 3
+	//自动返回 1，2，3，但是为了代码可读性，不建议这么写
+	return
+}
+
+// 不定长变参 , ...必须是参数列表最后一个参数
+func func_c(a ...int) {
+
+	fmt.Println(a)
+}
+
+func func_d(a *int) {
+	*a = 99
+	fmt.Println(*a)
+}
+
+func func_e() {
+	fmt.Println("func_e")
+}
+
+// 闭包
+func func_g(x int) func(int) int {
+	fmt.Println("外层x地址：", &x)
+	return func(y int) int {
+		fmt.Println("内层x地址：", &x)
+		return x + y
+	}
+}
